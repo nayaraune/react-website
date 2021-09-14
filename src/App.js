3b6e88f01f72eee93c83bfb761590a97fb5daa0f
Nayara Une <81695946+nayaraune@users.noreply.github.com>
2021-09-14 12:15:29 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Switch } from 'react-router';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+    <Router>
+     <Navbar />
+     <Switch>
+       <Route path='/' exact component={Home}/>
+       <Route path='/services'  component={Services}/>
+       <Route path='/products'  component={Products}/>
+       <Route path='/sign-up'  component={SignUp}/>
+     </Switch>
+     </Router>
+    </>
+  );
+}
+
+export default App;
